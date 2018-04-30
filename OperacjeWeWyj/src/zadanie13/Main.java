3ca1581dc56e0be40004398a8a8d8c87a3847fb4
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-30 07:29:23 +0200

    Zadanie 1.3, 1.4

---
+++ OperacjeWeWyj/src/zadanie13/Main.java
@@ -0,0 +1,24 @@
+package zadanie13; // Zadanie 1.3
+
+/**
+ * Napisz program, który wyświetla na ekranie komputera pierwiastek
+ * kwadratowy z wartości predefiniowanej π = 3,14…
+ * Należy przyjąć format wyświetlania pierwiastka kwadratowego
+ * ze stałej π z dokładnością do dwóch miejsc po przecinku.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program wyswietla pierwiastek kwadratowy ");
+        System.out.println("z liczby pi z dokładnością dwóch miejsc po przecinku.");
+        System.out.printf("Sqrt(Pi) = " + "%2.2f\n", Math.sqrt(Math.PI) );
+
+/**
+ * Metoda sqrt() pozwala na obliczenie pierwiastka kwadratowego. Jest
+ * ona metodą standardowej klasy Math.
+ */
+    }
+
+}
