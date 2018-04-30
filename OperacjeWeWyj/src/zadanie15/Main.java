119a9a4e48641e4d1cf7dced39426e444e16d4e8
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-30 08:03:50 +0200

    Zadanie 1.5 i 1.6

--- OperacjeWeWyj/src/zadanie15/Main.java
+++ OperacjeWeWyj/src/zadanie15/Main.java
@@ -1,4 +1,4 @@
-package zadanie15; // Zadanie 1.5
+package zadanie15; // Zadanie 1.5 plus 1.6
 /**
  *Napisz program, który oblicza wynik dzielenia całkowitego
  * bez reszty dwóch liczb całkowitych: a = 37 i b = 11.
@@ -17,5 +17,18 @@ public class Main
         System.out.println("bez reszty dwóch liczb całkowitych");
         System.out.println("Dla liczb a = " + a + ", b = " + b);
         System.out.println(a + " / " + b + " = " + a/b + ".");
+
+/**
+ * Napisz program, który oblicza resztę z dzielenia całkowitego
+ * dwóch liczb całkowitych: a = 37 i b = 11.
+ *
+ * Należy zastosować operator reszty z dzielenia całkowitego modulo, który
+ * oznaczamy w języku Java symbolem %. Podobnie jak w językach C i C++,
+ * operator ten umożliwia uzyskanie tylko reszty z dzielenia, natomiast
+ * wartość całkowita jest odrzucana.
+ */
+
+        System.out.println("Program oblicza resztę z dzielenia całkowitego. ");
+        System.out.println(a + " % " + b + " = " + a%b + ".");
     }
 }
