78f6b9d135e05918da81e22a33c661e3f9ce43cf
Krzysztof Kaczyński <qris70@gmail.com>
2018-04-30 06:22:09 +0200

    Add files via upload

---
+++ WitajNrazy.java
@@ -0,0 +1,12 @@
+package witajNrazy;
+
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Collections;
+
+public class WitajNrazy {
+
+    public static void main(String[] args) {
+        System.out.print(String.join("",Collections.nCopies(10, " \n DUPA ")));
+    }
+}
