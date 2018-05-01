dff653b5621514e42f4a803f7402feede88db8b1
[KrzysztofKaczyński] <qris70@gmail.com>
2018-05-01 08:24:20 +0200

    Zad 2.1

---
+++ InstrukcjeWarunkowe/src/zadanie21/Main.java
@@ -0,0 +1,48 @@
+package zadanie21; // zadanie 2.1
+
+/**
+ *Napisz program, który sprawdza dla trzech boków trójkąta a,
+ * b i c wprowadzonych z klawiatury, czy tworzą one trójkąt prostokątny
+ * (zakładamy, że a > 0, b > 0, c > 0).
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+/**
+ * W Javie istnieją dwie instrukcje warunkowe:
+ * 􀁔 instrukcja warunkowa if ... else,
+ * 􀁔 instrukcja wyboru switch ... case.
+ * Instrukcja if ... else służy do sprawdzania poprawności wyrażenia
+ * warunkowego i w zależności od tego, czy dany warunek jest prawdziwy,
+ * czy nie, pozwala na wykonanie różnych bloków programu.
+ * Jej ogólna postać jest następująca:
+ * if (warunek)
+ * {
+ * // instrukcje do wykonania, kiedy warunek jest prawdziwy
+ * }
+ * else
+ * {
+ * // instrukcje do wykonania, kiedy warunek jest fałszywy
+ * }
+ *
+ * Instrukcja wyboru switch ... case pozwala w wygodny i przejrzysty
+ * sposób sprawdzić ciąg warunków i wykonywać kod w zależności od
+ * tego, czy są one prawdziwe, czy fałszywe. Oto jej ogólna postać:
+ * switch (wyrażenie)
+ * {
+ * case wartość_1 : instrukcje_1;
+ * break;
+ * case wartość_2 : instrukcje_2;
+ * break;
+ * ........................
+ * case wartość_n : instrukcje_n;
+ * break;
+ * default : instrukcje;
+ * }
+ * Instrukcja break przerywa wykonywanie całego bloku case.
+  */
+
+
+    }
+}
