d3b2a4a11e3d6005624d30533fc0d65597b8cec4
[KrzysztofKaczyński] <qris70@gmail.com>
2018-05-04 19:33:02 +0200

    Zad for

---
+++ Iteracje/src/zadanie316/ForNajmniejszaNajwiekszaWylosowana.java
@@ -0,0 +1,11 @@
+package zadanie316; // Zadanie 316
+
+/**
+ * Napisz program, który za pomocą instrukcji for znajduje największą
+ * i najmniejszą liczbę ze zbioru n wylosowanych liczb
+ * całkowitych od 0 do 99 (w zadaniu n = 5) oraz oblicza średnią
+ * ze wszystkich wylosowanych liczb.
+ */
+
+public class ForNajmniejszaNajwiekszaWylosowana {
+}
